282309ec1ba25dda045b44218f41a08b97c4be8e
drrnmcdnld <drrnmcdnld@gmail.com>
2015-03-15 17:25:16 -0300

    Stripped out all the tab default data

---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('ergo.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('assessCtrl', function($scope) {})
+
+.controller('recsCtrl', function($scope) {});
