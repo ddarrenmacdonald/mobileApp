d7c6311793f944ce181156b35aae0007ba3f450b
drrnmcdnld <drrnmcdnld@gmail.com>
2015-03-17 02:15:19 -0300

    Updated Angular classes on the Assessment screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,46 @@ angular.module('ergo.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('AssessCtrl', function($scope) {})
+.controller('AssessCtrl', ['$scope' ,'$http',  function($scope,$http) {
+                $scope.val1 = 'This is temp Val' ;  
+                $scope.fName = '';
+                $scope.lName = '';
+                $scope.knee = '';  
+                $scope.elbow = '' ;
+                $scope.eye = '' ;
+                
+                $scope.getAllRec = function(){ 
+                     $http({method: 'GET', url: '/db/readRecords'}).
+                        success(function(data, status) { 
+                              $scope.dataset = data; 
+                        }).
+                        error(function(data, status) {
+                          $scope.dataset = data || "Request failed "; 
+                      }); 
+                }
+                
+                $scope.addRecord = function(){ 
+                    $http({method: 'GET', url: '/db/addRecord?fName='+$scope.fName+'&lName='+
+                           $scope.lName+'&knee='+$scope.knee+'&elbow='+$scope.elbow+'&eye='+$scope.eye}).
+                        success(function(data, status) { 
+                                alert('Record Added');
+                                $scope.getAllRec();
+                        });  
+                }
+                
+                $scope.delRecord = function(recId){
+                    console.log(recId);
+                    if(confirm('Are you sure you want to delete this record ? '))
+                    {
+                        $http({method: 'GET', url: '/db/delRecord?id='+recId}).
+                            success(function(data, status) {  
+                                    $scope.getAllRec();
+                            });
+                    }
+                } 
+                
+                $scope.getAllRec();
+                
+            }])
 
 .controller('RecsCtrl', function($scope) {});
