e3cfa976659a8d23e25a9d13e3af47e16f1af8fa
drrnmcdnld <drrnmcdnld@gmail.com>
2015-03-16 21:42:17 -0300

    Added custom splash images

--- www/js/server.js
+++ www/js/server.js
@@ -4,22 +4,7 @@ var pg = require('pg');
 var bodyParser = require('body-parser');
 
 // Connect to Postgres
-var conString = "postgres://dmac@localhost/ergo";
-
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
-}); 
+var client = new Client("postgres://dmac@localhost/ergo");
 
 // Create the Express application
 var app = express();
